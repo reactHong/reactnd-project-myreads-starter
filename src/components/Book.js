8329caf7b20e4085c9052ae7071d1299506a2920
Hong Shik Kim <hong@hongs.dev>
2020-12-31 23:35:43 +0900

    fix: Fix a bug when changing a book's shelf to another shelf on the search page

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import BookShelfSelector from './BookShelfSelector';
 
 class Book extends Component {
   static propTypes = {
@@ -9,22 +8,43 @@ class Book extends Component {
     onFindShelf: PropTypes.func,
   }
 
+  state = {
+    shelf: "none"
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("\n[Book.getDerivedStateFromProps] nextProps.book.shelf", nextProps.book.shelf, nextProps.book.id);
+
+    let shelf = nextProps.book.shelf;
+    if (!shelf && nextProps.onFindShelf) {
+      shelf = nextProps.onFindShelf(nextProps.book.id);
+    }
+
+    return {
+      shelf: shelf,
+    };
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   //TODO: Don't render if it is same book
+
+  //   return true;
+  // }
+
   changeShelf = (e) => {
     console.log("[Book.changeShelf]");
     console.log(e.target.value);
     
     const copiedBook = JSON.parse(JSON.stringify(this.props.book));
-    const fromShelf = copiedBook.shelf;
+    const fromShelf = this.state.shelf;
     const toShelf = e.target.value;
     this.props.onMoveTo(copiedBook, fromShelf, toShelf);
-    this.setState({ bookshelf: toShelf });
   }
 
   render() {
     const book = this.props.book;
     const title = book.title;
     const authors = book.authors ? book.authors.join(", ") : "No Author";
-    const shelf = book.shelf ? book.shelf : "none";
     let imageURL = "";
     if (book.imageLinks) {
       imageURL = book.imageLinks.thumbnail ? 
@@ -33,18 +53,20 @@ class Book extends Component {
                           `url(${book.imageLinks.smallThumbnail})` : "";
     }
 
-    console.log("\n[Book.render] id:", book.id, book.title);
+    console.log(`[Book.render] ${book.title} -`, this.state.shelf, `- ${book.id}`);
     return(
       <li>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: imageURL }}></div>
             <div className="book-shelf-changer">
-              <BookShelfSelector 
-                bookid={book.id}
-                shelf={shelf} 
-                onChangeShelf={this.changeShelf} 
-                onFindShelf={this.props.onFindShelf}/>
+            <select value={this.state.shelf} onChange={this.changeShelf}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
             </div>
           </div>
           <div className="book-title">{title}</div>
