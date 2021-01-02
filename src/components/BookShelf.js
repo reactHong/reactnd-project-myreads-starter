a4b81602da06aefbd0f5ae1f7c5eb00863b5416e
Hong Shik Kim <hong@hongs.dev>
2021-01-03 00:36:00 +0900

    refactor: Change the class component to the functional component
    The state is not needed.

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 import Loader from 'react-loader-spinner';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
-class BookShelf extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array.isRequired,
-    isLoading: PropTypes.bool.isRequired,
-    onMoveTo: PropTypes.func.isRequired,
-  }
-  
-  render() {
-    return(
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
-        <div className="bookshelf-books">
-          {this.props.isLoading ? 
-            (<div>
-              <Loader 
-                type="ThreeDots"
-                color="#00BFFF"
-                height={100}
-                width={100}
-                timeout={0}
+function BookShelf(props) {
+  return(
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        {props.isLoading ? 
+          (<div>
+            <Loader 
+              type="ThreeDots"
+              color="#00BFFF"
+              height={100}
+              width={100}
+              timeout={0}
+            />
+           </div>) : 
+          (<ol className="books-grid">
+            {props.books.map((book, index) => (
+              <Book 
+                key={index} 
+                book={book} 
+                onMoveTo={props.onMoveTo}
               />
-             </div>) : 
-            (<ol className="books-grid">
-              {this.props.books.map((book, index) => (
-                <Book 
-                  key={index} 
-                  book={book} 
-                  onMoveTo={this.props.onMoveTo}
-                />
-              ))}
-            </ol>)}
-        </div>
+            ))}
+          </ol>)}
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+BookShelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  onMoveTo: PropTypes.func.isRequired,
 }
 
 export default BookShelf;
